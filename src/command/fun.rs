dc92d9989bf1e7922a56da77d6a655442ae3a91b
shibedrill <53824200+shibedrill@users.noreply.github.com>
2024-04-16 20:43:27 -0400

    nothing

--- src/command/fun.rs
+++ src/command/fun.rs
@@ -5,6 +5,7 @@ use crate::Error;
 /// MAKE HER BLEAT
 #[poise::command(slash_command)]
 pub async fn bleat(ctx: Context<'_>) -> Result<(), Error> {
+
     ctx.say("BLEAT TEST")
     .await?;
     info!("Executed command `bleat` successfully");
